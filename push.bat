76d9060a9469e1a4c6f20ae11c108f3c0873c8a1
sihyun <gthpgth@gmail.com>
2022-05-05 17:18:01 +0900

    autoCommit 5-052-20.171801

--- push.bat
+++ push.bat
@@ -1,4 +1,18 @@
+@echo off
+setlocal enabledelayedexpansion
+
 cd /D "%~dp0"
+
+for /f "delims=" %%i in ('git status -s') do (
+   set "tmp=%%i"
+   set "tmp=!tmp:~2!"
+   set "result=!result!,!tmp!"
+)
+
+IF "%result:~2%" == "" GOTO END
+
 git add *
 git commit -m "autoCommit %date:~-4%%date:~3,2%%date:~0,2%.%time:~0,2%%time:~3,2%%time:~6,2%"
 git push origin main
+
+:END
